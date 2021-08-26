33f14c98172a79eb8f8366e25416321edc1be311
marcowang01 <marcowang01@gmail.com>
2021-08-27 02:03:38 +0800

    use AudioSegment

--- newPyDiarization.py
+++ newPyDiarization.py
@@ -3,48 +3,59 @@ from pyannote.audio.features import RawAudio
 import torch
 from pyAudioAnalysis import audioBasicIO
 import numpy as np
-
+from pydub import AudioSegment
+from scipy.io.wavfile import read
 import soundfile as sf
 import csv
 
 
-def speakerDiarization(x, fs, filename, chunkNum, chunkLength=60):
+def speakerDiarization(x, sample_width, sample_rate, n_channels, path, chunk_num, chunk_length=60):
     '''
         ARGUMENTS:
-            - x             the audio signal as a byte array
-            - fs            the sampling rate of the audio passed in
-            - filename      the name of the WAV file to store the audio
-            - chunkNum      the number of times the audio has already been processed
-            - chunkLength   the interval in seconds between each time the audio is processed
+            - data          the audio signal as a byte array (np ndarray)
+            - sample_width  the sample_width (bytes) of the audio signal
+            - sample_rate   the sampling rate (Hz) of the audio signal
+            - n_channels     the number of channels of the audio signal
+            - path          the path of the WAV file to store the audio
+            - chunk_num      the number of times the audio has already been processed;
+                            initiate at 0, pass in returned value afterwards
+            - chunk_length   the interval in seconds between each time the audio is processed
     '''
 
-    if chunkNum == 0:
-        sf.write(filename, x, fs)
+    if chunk_num == 0:
+        new = AudioSegment(data=x.tobytes(),
+                           sample_width=sample_width,
+                           frame_rate=sample_rate,
+                           channels=n_channels)
+        new.export(path, format='wav')
 
-    [signal, sampling_rate] = sf.read(filename)
+    audio = AudioSegment.from_file(path, format='wav')
 
-    if chunkNum != 0:
-        signal = np.concatenate((signal, x))
-        sf.write(filename, signal, sampling_rate)
-    chunkNum = 1 if chunkNum == 0 else chunkNum
+    if chunk_num != 0:
+        new = AudioSegment(data=x.tobytes(),
+                           sample_width=audio.sample_width,
+                           frame_rate=audio.frame_rate,
+                           channels=audio.channels)
+        # append new audio to old audio stored on file
+        audio = audio + new
+        audio.export(path, format='wav')
 
-    signal = audioBasicIO.stereo_to_mono(signal)
-    duration = len(audioBasicIO.stereo_to_mono(signal)) / sampling_rate
+    chunk_num = 1 if chunk_num == 0 else chunk_num
+
+    monoAudio = audio.set_channels(1)
+    duration = audio.duration_seconds
 
     # no processing occurs if interval is not reached, audio is stored as wav file
-    if duration < chunkLength * chunkNum:
+    if duration < chunk_length * chunk_num:
         print("Need more audio first")
-        return -1, -1, chunkNum
+        return -1, -1, chunk_num
 
-    chunkedFilename = f'{filename[:len(filename) - 4]}_byChunk.wav'
-
-    waveform = RawAudio(sample_rate=sampling_rate)\
-        .crop({'audio': filename}, Segment(0, chunkNum * chunkLength))
-    sf.write(chunkedFilename, waveform, sampling_rate)
+    monoPath = f'mono_{path}'
+    audio.export(monoPath, format='wav')
 
     pipeline = torch.hub.load('pyannote/pyannote-audio', 'dia')  # can use 'dia' or 'dia_ami'
     print('diarization begins')
-    diarization = pipeline({'audio': chunkedFilename})
+    diarization = pipeline(dict(audio=monoPath))
 
     speakers = set()
     timings = []
@@ -57,7 +68,7 @@ def speakerDiarization(x, fs, filename, chunkNum, chunkLength=60):
             'end': turn.end
         })
 
-    return len(speakers), timings, (chunkNum + 1)  # refer to csv file for return values
+    return len(speakers), timings, (chunk_num + 1)  # refer to csv file for return values
 
 
 # testing code
@@ -67,8 +78,8 @@ if __name__ == "__main__":
     for i in range(7):
         print(f'--ROUND {i}--')
         f_name = f'chunks/chunk{i}.wav'
-        [np_array, rate] = sf.read(f_name)
-        speakers, timings, chunkNum = speakerDiarization(np_array, 44100, 'testAudio.wav', chunkNum)
+        rate, signal = read(f_name)
+        speakers, timings, chunkNum = speakerDiarization(signal, 2, 44100, 2, 'testAudio.wav', chunkNum)
 
         print(f'chunk no.: {chunkNum}\nspeakers: {speakers}\n{timings}\n')
 
